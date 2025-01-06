1734cd1e8a866e20c828292e2b3da7ce381752e4
tahmidchy <Tahmidchy@users.noreply.github.com>
2025-01-06 10:06:51 +0600

    Tahmid Chowdhury Challenge Type- Easy
    Exercism challenge here .. this is the first challenge...

---
+++ Challenge1_Hello_World/challenge1.js
@@ -0,0 +1,19 @@
+// Challenges - 1
+/*
+
+The classical introductory exercise. Just say "Hello, World!".
+
+"Hello, World!" is the traditional first program for beginning programming in a new language or environment.
+
+The objectives are simple:
+
+Modify the provided code so that it produces the string "Hello, World!".
+Run the test suite and make sure that it succeeds.
+
+*/
+
+//Solution:
+
+export function hello(){
+  return "Hello, World!";
+}
