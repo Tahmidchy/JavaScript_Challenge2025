af901380c0f6c5f5c9e0ffed4550291c52060633
A.T.M.MUSTAFA MASUD CHOWDHURY <Tahmidchy@users.noreply.github.com>
2025-11-29 13:51:49 +0600

    Tahmid Chowdhury with Js Loop2
    Here, we are solving a loop problem using JavaScript. Loop 2.

--- Problem13.js
+++ Problem13.js
@@ -1,9 +1,15 @@
 /*
-TODO: Problem-13: There is an array.language =["JavaScript", "Python", "Java", "C++"] This array will convert each language name into a string and add a semicolon between each one.
+TODO: Problem-13: You will list the roll numbers of students from 1 to 50 in a school and then use a While loop to find the sum of those numbers.
 */
 
 // Solution:
 
-let languages = ["JavaScript", "Python", "Java", "C++"];
-let languagesString = languages.join("; ");
-console.log(languagesString); // Output: "JavaScript; Python; Java; C++"
+let rollNumber = 1; // Initialize a counter variable starting from 1
+let sum = 0; // Initialize a variable to hold the sum of the roll numbers
+while(rollNumber <= 50){
+    sum += rollNumber; // Add the current value of the counter variable to the sum
+    rollNumber++; // Increment the counter variable by 1
+    // The loop will continue until the counter variable is less than or equal to 50
+    // This will calculate the sum of the roll numbers from 1 to 50
+}
+console.log(`The sum of roll numbers from 1 to 50 is: ${sum}`); // Log the final sum to the console
--- Problem16.js
+++ Problem16.js
@@ -1,12 +1,11 @@
 /*
-TODO: Problem-16:  const str1 = "Push", const str2 = "Pop", const str3 = "shift", const str4 = "unshift" Use concat to create a string by combining these strings.
+TODO: Problem 16: Print from 150 to 170 using a for loop.
 */
 
-// Solution: 
+// Solution:
+for (let i = 150; i <= 170; i++){
+    console.log(`The number is: ${i}`);
+}
 
-const str1 = "Push";
-const str2 = "Pop"; 
-const str3 = "shift";
-const str4 = "unshift";
-const combinedStr = str1.concat(" ", str2, " ", str3, " ", str4);
-console.log(combinedStr); // Output: "Push Pop shift unshift"
+// Output:
+// The number is: 150 
