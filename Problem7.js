7c72291cfcaa0d4e5a03a6892137cc88728179f2
A.T.M.MUSTAFA MASUD CHOWDHURY <Tahmidchy@users.noreply.github.com>
2026-01-11 09:59:04 +0600

    Tahmid Chowdhury With JSON
    This is Json Problem

--- Problem7.js
+++ Problem7.js
@@ -1,9 +1,25 @@
-/* 
-TODO: Problem-7: Declare a variable named age to hold your current age, then print its value to the console. Next, assign a new value to the age variable and print it again.
- */
+/*
+TODO: Problem-7: Create some data for courses for the learning platform, where there will be minimum 3 courses, each course will have course title, instructors name, duration and lesson array, lesson array will have lesson name, duration and difficulty level, difficulty level will be any one of beginner, intermediate, advanced, convert it with JSON .stringify
+*/
 
-//Solution
-let age = 25;
-console.log(age);
-age = 30;
-console.log(age);
+// Solution: 
+
+const learningPlatform = {
+    course : [
+        {
+            courseTitle: 'JavaScript Basics',
+            instructorName: 'John Doe',
+            duration: '4 weeks',
+            lessons: ['Variables and Data Types', 'Functions and Scope', 'DOM Manipulation'],
+        },
+        {
+            courseTitle: 'Advanced Python',
+            instructorName: 'Jane Smith',
+            duration: '6 weeks',
+            lessons: ['Decorators and Generators', 'Concurrency', 'Web Development with Django'],
+        }
+
+    ]
+}
+const learningPlatformJSON = JSON.stringify(learningPlatform);
+console.log(learningPlatformJSON);
